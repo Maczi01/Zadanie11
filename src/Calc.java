5676318e028882ee54c6a2c1776c972a70371d24
Maczi01 <system11>
2019-01-27 00:13:28 +0100

    Second commit. Bug fixes.

--- src/Calc.java
+++ src/Calc.java
@@ -1,18 +1,16 @@
 public class Calc {
 
-    String whichPart(Point point){
-        String text1 = "Punkt lezy w ";
-        String text2 = " cwiartce ukladu wspolrzednych";
+    int whichPart(Point point){
 
         if (point.getPointX() >= 0 && point.getPointY() >= 0) {
-            return text1 + "I"+text2;
+            return 1;
         }
         if (point.getPointX() < 0 && point.getPointY() >= 0) {
-            return text1 + "II"+text2;
+            return 2;
         }
         if (point.getPointX() < 0 && point.getPointY() < 0) {
-            return text1 + "III"+text2;
+            return 3;
         } else
-            return text1 + "IV"+text2;
+            return 4;
     }
 }
--- src/InputOutput.java
+++ src/InputOutput.java
@@ -3,18 +3,34 @@ import java.util.Scanner;
 public class InputOutput {
 
     Scanner scanner = new Scanner(System.in);
-
+    Point p = new Point();
+    Calc calc = new Calc();
+    
     Point loadPoints() {
-        Point p = new Point();
         int x = 0;
         int y;
         System.out.println("Podaj punkt X");
         x = scanner.nextInt();
-
         System.out.println("Podaj punkt Y");
         y = scanner.nextInt();
         p.setPointX(x);
         p.setPointY(y);
+        showInfo(calc);
         return new Point(x,y);
+    }
+
+    void showInfo(Calc calc){
+        if (calc.whichPart(p)==1){
+            System.out.println("1 ćwiartka");
+        }
+        if (calc.whichPart(p)==2){
+            System.out.println("2 ćwiartka");
+        }
+        if (calc.whichPart(p)==3){
+            System.out.println("3 ćwiartka");
+        }
+        if (calc.whichPart(p)==4){
+            System.out.println("4 ćwiartka");
+        }
     }
 }
